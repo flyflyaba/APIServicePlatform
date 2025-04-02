554b476a8e34b15223efd19b8de4c0c4c4ab7b79
soar <1147358383@qq.com>
2025-04-02 10:22:43 +0800

    'add_edit_delete_interface'

--- feiapi-backend/src/main/java/com/fei/project/service/impl/InterfaceInfoServiceImpl.java
+++ feiapi-backend/src/main/java/com/fei/project/service/impl/InterfaceInfoServiceImpl.java
@@ -31,7 +31,7 @@ public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, I
             }
         }
         // 有参数则校验
-        if (StringUtils.isNotBlank(name) && name.length() < 50) {
+        if (StringUtils.isNotBlank(name) && name.length() > 50) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "名称过长");
         }
     }
